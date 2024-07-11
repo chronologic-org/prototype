7be9c7da290c3a8c2375235d544a3d7c70ac3bd6
Kristiana Karshelieva <kris.krsh02@gmail.com>
2024-07-11 19:40:51 +0300

    base calendar class

---
+++ calendar_clients/base.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+class Calendar(ABC):
+    @abstractmethod
+    def create_event(self, calendar_id, event):
+        pass
+
+    @abstractmethod
+    def delete_event(self, calendar_id, event_id):
+        pass
+
+    @abstractmethod
+    def update_event(self, calendar_id, event_id, event):
+        pass
