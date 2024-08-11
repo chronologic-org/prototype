df9fcba9f03f5627c968be3b50794d501fcdc184
Kristiana Karshelieva <kris.krsh02@gmail.com>
2024-08-11 10:59:09 +0300

    authorization from sidebar

--- streamlit_UI.py
+++ streamlit_UI.py
@@ -1,11 +1,19 @@
 import streamlit as st
 import requests
 
+def initiate_authorization():
+    auth_url = 'http://localhost:5000/authorize/google'
+    st.markdown(f"""
+        <meta http-equiv="refresh" content="0; url={auth_url}">
+        If you are not redirected automatically, <a href="{auth_url}">click here</a>.
+    """, unsafe_allow_html=True)
+    
 def get_token_from_url():
-    query_params = st.experimental_get_query_params()
-    token = query_params.get('token', [None])[0]
+    query_params = st.query_params
+    token = query_params.get('token', None)
     if token:
         st.session_state['token'] = token
+        st.query_params.clear()
 
 def get_events():
     headers = {'Authorization': f"Bearer {st.session_state['token']}"}
@@ -22,5 +30,15 @@ if 'token' not in st.session_state:
 
 if 'token' in st.session_state:
     st.button('Get Google Calendar Events', on_click=get_events)
-else:
-    st.markdown("[Login with Google](http://localhost:5000/authorize/google)")
+
+
+with st.sidebar:
+    if 'token' in st.session_state:
+        st.write('Logged in to Google Calendar') 
+        if st.button('Logout'):
+            st.session_state.pop('token')
+            st.query_params.clear()
+    else:
+        st.write('Log in to Google Calendar')
+        if st.button('Login'):
+            initiate_authorization()
